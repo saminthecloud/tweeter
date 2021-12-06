c9a20f8936aba96de9ba5506a5245a194062a3c0
saminthecloud <samrat.oommen@outlook.com>
2021-12-06 03:30:58 +0000

    Displayed validation errors with jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
 
+const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
   
+
   const createTweetElement = function(tweet) {
     let $tweet = 
     `<article class="tweet">
@@ -38,7 +20,7 @@
       <span class="tweet-name">${tweet.user.name}</span>
       <span class="tweet-handle">${tweet.user.handle}</a></span>
     </header>
-    <p class="tweet-text">${tweet.content.text}</p>
+    <p class="tweet-text">${escape(tweet.content.text)}</p>
     <footer>
       <span class="date-tweeted">${timeago.format(tweet.created_at)}</span>
       <span class="actions">
@@ -61,20 +43,31 @@
       $.get("/tweets", {}, function(data, textStatus, jqXHR) {
         if (textStatus === 'success') {
             renderTweets(data);
-            console.log('Statusc:', textStatus);
-            console.log("Datac:", data);
+            console.log('Status:', textStatus);
+            console.log("Data:", data);
         } else alert ("Error retrieving tweets. Status code:", textStatus);
         
       })
   }
 
   $(document).ready(() => {
+    $('#error-message').hide();
     loadTweets();
 
     $("#target").submit( function (event) {
         event.preventDefault();
-        $.post("/tweets", $("#target").serialize(), (data) => {
-            $('#tweet-text').val("")
-        })
-        })
+        if ($('#tweet-text').val().length === 0) {
+            $('#error-message').text("Empty tweet!");
+            $('#error-message').show();
+        } else if ($('#tweet-text').val().length > 140) {
+            $('#error-message').text("Tweet is more than 140 characters!");
+            $('#error-message').show();
+        } else {
+            $.post("/tweets", $("#target").serialize(), (data) => {
+                $('#tweet-text').val("");
+                $('#error-message').hide();
+                loadTweets();
+            })
+        }
+    })
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $("textarea").keyup(function() {
         const tweetLength = $(this).val().length; 
-        $(".counter").text(140-tweetLength);
+        $("#counter").text(140-tweetLength);
         if (tweetLength > 140) {
-            $('.counter').css("color", "red");
+            $('#counter').css("color", "red");
         } else {
-            $('.counter').css("color", "black");
+            $('#counter').css("color", "black");
+            $('#error-message').hide();
         }
     })
 }); 
