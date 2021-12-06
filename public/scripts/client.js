4b99ae7a3489a06cc02fd6103220737d18e4114d
saminthecloud <samrat.oommen@outlook.com>
2021-12-06 02:05:23 +0000

    Fetching tweets from server. Ajax issue fixed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
 
   
   const createTweetElement = function(tweet) {
@@ -57,14 +57,24 @@ const data = [
     }
   }
 
+  const loadTweets = function() {
+      $.get("/tweets", {}, function(data, textStatus, jqXHR) {
+        if (textStatus === 'success') {
+            renderTweets(data);
+            console.log('Statusc:', textStatus);
+            console.log("Datac:", data);
+        } else alert ("Error retrieving tweets. Status code:", textStatus);
+        
+      })
+  }
+
   $(document).ready(() => {
-    renderTweets(data);
+    loadTweets();
+
+    $("#target").submit( function (event) {
+        event.preventDefault();
+        $.post("/tweets", $("#target").serialize(), (data) => {
+            $('#tweet-text').val("")
+        })
+        })
 })
-
-
-$("#target").submit( function (event) {
-    event.preventDefault();
-    $.post("/tweets", $("#target").serialize(), (data) => {
-        $('#tweet-text').val("")
-    })
-    })
